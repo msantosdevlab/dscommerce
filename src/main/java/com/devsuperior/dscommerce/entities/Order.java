8da99107826ff854f0278492f7bcd0b4a401e087
msantos-lab <msantosdev.lab@gmail.com>
2024-02-23 14:15:38 +0100

    New Files: Category, Produtc, Oder, OrderItem e OrderItemPK

--- src/main/java/com/devsuperior/dscommerce/entities/Order.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Order.java
@@ -1,6 +1,9 @@
 package com.devsuperior.dscommerce.entities;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.List;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -10,6 +13,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
@@ -30,10 +34,11 @@ public class Order {
     @JoinColumn(name = "client_id")
     private User client;
 
-
     @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
     private Payment payment;
- 
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Order() {
     }
@@ -45,7 +50,6 @@ public class Order {
         this.client = client;
         this.payment = payment;
     }
-    
 
     public Long getId() {
         return id;
@@ -77,6 +81,22 @@ public class Order {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Product> getProducts() {
+        return items.stream().map(x -> x.getProduct()).toList();
     }
 
 }
