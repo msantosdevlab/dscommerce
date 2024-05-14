e0091c4ea0edd24245ec0b56c110ef784ece95ff
msantos-lab <msantosdev.lab@gmail.com>
2024-05-14 21:39:43 +0200

    Saving Order

--- src/main/java/com/devsuperior/dscommerce/dto/OrderDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/OrderDTO.java
@@ -7,62 +7,71 @@ import com.devsuperior.dscommerce.entities.Order;
 import com.devsuperior.dscommerce.entities.OrderItem;
 import com.devsuperior.dscommerce.entities.OrderStatus;
 
+import jakarta.validation.constraints.NotEmpty;
+
 public class OrderDTO {
 
     private Long id;
-    private Instant moment;
-    private OrderStatus status;
+	private Instant moment;
+	private OrderStatus status;
+	
+	private ClientDTO client;
+	
+	private PaymentDTO payment;
+	
+	@NotEmpty(message = "Deve ter pelo menos um item")
+	private List<OrderItemDTO> items = new ArrayList<>();
 
-    private ClientDTO client;
+	public OrderDTO(Long id, Instant moment, OrderStatus status, ClientDTO client, PaymentDTO payment) {
+		this.id = id;
+		this.moment = moment;
+		this.status = status;
+		this.client = client;
+		this.payment = payment;
+	}
+	
+	public OrderDTO(Order entity) {
+		this.id = entity.getId();
+		this.moment = entity.getMoment();
+		this.status = entity.getStatus();
+		this.client = new ClientDTO(entity.getClient());
+		this.payment = (entity.getPayment() == null) ? null : new PaymentDTO(entity.getPayment());
+		for (OrderItem item : entity.getItems()) {
+			OrderItemDTO itemDto = new OrderItemDTO(item);
+			items.add(itemDto);
+		}
+ 	}
 
-    private PaymentDTO payment;
+	public Long getId() {
+		return id;
+	}
 
-    private List<OrderItemDTO> items = new ArrayList<>();
+	public Instant getMoment() {
+		return moment;
+	}
 
-    public OrderDTO(Order entity) {
-        id = entity.getId();
-        moment = entity.getMoment();
-        status = entity.getStatus();
-        client = new ClientDTO(entity.getClient());
-        payment = (entity.getPayment() == null ? null : new PaymentDTO(entity.getPayment()));
-        for(OrderItem item : entity.getItems()){
-            OrderItemDTO itemDto = new OrderItemDTO(item);
-            items.add(itemDto);
+	public OrderStatus getStatus() {
+		return status;
+	}
 
-        }
-    }
+	public ClientDTO getClient() {
+		return client;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public PaymentDTO getPayment() {
+		return payment;
+	}
 
-    public Instant getMoment() {
-        return moment;
-    }
-
-    public OrderStatus getStatus() {
-        return status;
-    }
-
-    public ClientDTO getClient() {
-        return client;
-    }
-
-    public PaymentDTO getPayment() {
-        return payment;
-    }
-
-    public List<OrderItemDTO> getItems() {
-        return items;
-    }
-
-    public Double getTotal(){
-        double sum = 0.0;
-        for(OrderItemDTO item : items){
-            sum += item.getSubtotal();
-        }
-
-        return sum;
-    }
+	public List<OrderItemDTO> getItems() {
+		return items;
+	}
+	
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItemDTO item : items) {
+			sum += item.getSubtotal();
+		}
+		return sum;
+	}
 
 }
