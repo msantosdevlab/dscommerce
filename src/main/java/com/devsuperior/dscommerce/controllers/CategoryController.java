d441087702bafb9e9f79a955a9f542b5b99a350e
msantos-lab <msantosdev.lab@gmail.com>
2024-05-14 22:36:46 +0200

    GET Categories

---
+++ src/main/java/com/devsuperior/dscommerce/controllers/CategoryController.java
@@ -0,0 +1,30 @@
+package com.devsuperior.dscommerce.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dscommerce.dto.CategoryDTO;
+import com.devsuperior.dscommerce.services.CategoryService;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService service;
+    
+
+    //@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> findAll() {
+        List<CategoryDTO> dto = service.findAll();
+        return ResponseEntity.ok(dto);
+    }
+
+}
